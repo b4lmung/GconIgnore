35e5b0730473fe429928bb8077b6c175e991bc7a
b4lmung <b4lmung.m@gmail.com>
2016-05-27 02:05:58 +0900

    Update gconIgnore.user.js

--- gconIgnore.user.js
+++ gconIgnore.user.js
@@ -16,6 +16,7 @@ var allPosts = document.getElementsByClassName("comment-block");
 if(allPosts.length > 0){
   var targets = [];
   for(i=0; i<allPosts.length; i++){
+     
       var user =  allPosts[i].getElementsByClassName("name")[0].textContent;
      
       if(user.contains("member"))
@@ -25,12 +26,18 @@ if(allPosts.length > 0){
         user = user.substring(0, user.lastIndexOf("staff"));
 
       if(blocklist.indexOf(user) > -1){
-        var postElement = allPosts[i].getElementsByClassName("reply-block")[0];
-        var html = postElement.innerHTML;
-        var likeText = html.substring(html.indexOf("<div class=\"like\""));
-        var postText = html.substring(0, html.indexOf("<div class=\"like\""));
         cnt++;
-        postElement.innerHTML = "<input name=\"spoilbutton\" value=\"ข้อความนี้ถูกซ่อนไว้ เนื่องจากเป็นโพสของผู้ไม่พึงประสงค์  กดที่ปุ่มนี้เพื่อเปิด/ปิดข้อความ\" onclick=\"toggleBox('block" + cnt + "', 1);\" type=\"button\"/><div id=\"block" + cnt + "\" style=\"visibility:hidden;display:none;\">" + postText + "</div>" + likeText;
+
+        allPosts[i].id = "block" + cnt;
+        allPosts[i].style.visibility = "hidden";
+        allPosts[i].style.display = 'none';
+        var button = document.createElement('input');
+        button.type = "button";
+        button.style = 'width:95%; height:50px; margin: 20px';
+        button.setAttribute("onclick","toggleBox('block" + cnt + "', 1);");
+        button.value = "ข้อความนี้ถูกซ่อนไว้ เนื่องจากเป็นโพสของผู้ไม่พึงประสงค์ (" + user + ") กดที่ปุ่มนี้เพื่อเปิด/ปิดข้อความ";
+        allPosts[i].parentNode.insertBefore(button, allPosts[i]);
+        
       }  
    }
   
