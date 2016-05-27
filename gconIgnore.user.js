7fd2819198a376532f1f605ff74ef888678ab70b
b4lmung <b4lmung.m@gmail.com>
2016-05-27 15:01:28 +0900

    Update gconIgnore.user.js

--- gconIgnore.user.js
+++ gconIgnore.user.js
@@ -17,6 +17,7 @@ if(allPosts.length > 0){
   var targets = [];
   for(i=0; i<allPosts.length; i++){
      
+        
       var user =  allPosts[i].getElementsByClassName("name")[0].textContent;
      
       if(user.contains("member"))
@@ -37,12 +38,33 @@ if(allPosts.length > 0){
         button.setAttribute("onclick","toggleBox('block" + cnt + "', 1);");
         button.value = "ข้อความนี้ถูกซ่อนไว้ เนื่องจากเป็นโพสของผู้ไม่พึงประสงค์ (" + user + ") กดที่ปุ่มนี้เพื่อเปิด/ปิดข้อความ";
         allPosts[i].parentNode.insertBefore(button, allPosts[i]);
-        
       }  
+        
+        
+      //search quote
+      var quoteTitles = allPosts[i].getElementsByClassName("quote-title");
+      for(j=0; j<quoteTitles.length; j++){
+           
+           var n = quoteTitles[j].textContent;
+           n = n.substring(0, n.lastIndexOf(" wrote:"))
+           if(blocklist.indexOf(n) > -1){ 
+                var quote = quoteTitles[j].parentNode;
+                cnt++;
+                alert(n + "\t" + user);
+             
+                quote.id = "block" + cnt;
+                quote.style.visibility = "hidden";
+                quote.style.display = 'none';
+                var button = document.createElement('input');
+                button.type = "button";
+                button.style = 'width:95%; height:50px; margin: 20px';
+                button.setAttribute("onclick","toggleBox('block" + cnt + "', 1);");
+                button.value = "ข้อความนี้ถูกซ่อนไว้ เนื่องจากมีการ Quote โพสของผู้ไม่พึงประสงค์ (" + n + ") กดที่ปุ่มนี้เพื่อเปิด/ปิดข้อความ";
+                quote.parentNode.insertBefore(button, quote);
+  
+           }  
+      }
+    
    }
   
 }
-
-
-
- 
